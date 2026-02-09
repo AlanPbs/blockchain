82f0207466ee090eb0253b882776b45070486762
Nathan <nathan.casabieille@epitech.eu>
2026-02-09 17:41:12 +0100

    fix: fixed chain id

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -5,6 +5,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const SEPOLIA_RPC = process.env.SEPOLIA_RPC || "https://ethereum-sepolia-rpc.publicnode.com";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -17,6 +18,11 @@ const config: HardhatUserConfig = {
       url: "http://127.0.0.1:8545",
       accounts: PRIVATE_KEY ? [PRIVATE_KEY] : undefined,
     },
+    sepolia: {
+      url: SEPOLIA_RPC,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 11155111
+    }
   },
 };
 
